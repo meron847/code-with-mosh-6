c653158c6e277e9cfef936a3af28ca7fd9b6d683
meron847 <belachewmeron15@gmail.com>
2025-07-26 15:21:13 +0100

    useing join

---
+++ joining array/index.js
@@ -0,0 +1,11 @@
+let numbers=[1,2,3,4]
+const separat=numbers.join('-');
+console.log(separat);
+
+
+const message='this is my first message';
+const text = message.split('  ');
+console.log(text)
+
+const parts=separat.join('--')
+console.log(parts)
