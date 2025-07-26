e4196a872f9bba7f2372345f8d2561f96f5702cd
meron847 <belachewmeron15@gmail.com>
2025-07-26 11:45:18 +0100

    removing element

---
+++ removing elements/index.js
@@ -0,0 +1,13 @@
+let number=[1,2,3,4,5]
+//Remove last number
+/*     const end=number.pop();
+console.log(end)
+console.log(number)
+//Remove beginning
+const first=number.shift();
+console.log(first)
+console.log(number)   */
+//Remove  middle
+const middle=number.splice(2,1);
+console.log(middle);
+console.log(number)
