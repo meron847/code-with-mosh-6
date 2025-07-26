5fe35c72685baf6c52d846a627c6699c5f4176f4
meron847 <belachewmeron15@gmail.com>
2025-07-26 12:07:38 +0100

    emptying an array

---
+++ emptying an array/index.js
@@ -0,0 +1,9 @@
+let number=[1,2,3,4,5]
+let another = number;
+
+//solution 1 we use this solution when we have single reference only
+//number=[];
+
+//solution 2 
+number.length=0;
+console.log(number)
