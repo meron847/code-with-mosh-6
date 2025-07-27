08b79a1b3e3188850ee1b3d95767ba73d1379618
meron847 <belachewmeron15@gmail.com>
2025-07-27 15:24:34 +0100

    scope

---
+++ scope,local or global/index.js
@@ -0,0 +1,13 @@
+//global scope
+const color='red'
+function start(){
+    const message ='hi';
+    console.log(color);
+}
+//local scope
+
+/*function stop(){
+    const message ='hi';
+    const bold='red'
+    console.log(bold)
+}*/
