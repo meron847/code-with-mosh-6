9a4f956a7df6979daeb4cf0a3b47bd453f4b5b5d
meron847 <belachewmeron15@gmail.com>
2025-07-28 10:01:45 +0100

    object literal

---
+++ part 2/index.js
@@ -0,0 +1,16 @@
+/* what is object-oriented programming?
+   is a programming paradigm centerd around objects rather than functions   */
+   //serch 4 pillary of obeject oriented programming
+
+   const circle={
+    radius:2,
+    location:{
+        x:1,
+        Y:2,
+    },
+    draw: function(){
+        console.log('draw')
+    }
+   };
+  circle.draw();
+  //object literals is creating an object with currly braces with a key value pair inside
