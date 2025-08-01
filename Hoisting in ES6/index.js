d255c1cfcbe32c02ec990aaa595768b270b676f8
meron847 <belachewmeron15@gmail.com>
2025-08-01 09:47:38 +0100

    hoisting in ES6

---
+++ Hoisting in ES6/index.js
@@ -0,0 +1,8 @@
+sayHellow();
+//Function Declaration
+function sayHellow() {}//you can call function declaration.
+
+//Function Expression
+const sayGoodbye = function() {};//so function expresion theye didnot hoisting
+
+//we should declare first not before any thigh thire d/f is declaration have no semicolon and expression have semi colon
