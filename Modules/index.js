de4d05f25d738ebc73d6ab766c50ccfc20ae8e20
meron847 <belachewmeron15@gmail.com>
2025-08-02 14:42:05 +0100

    modules in ES6

---
+++ Modules/index.js
@@ -0,0 +1,5 @@
+//A module is like a separate file that contains code you can use in your program.
+//It helps you organize your code into smaller, manageable pieces instead of keeping everything in one big file.
+import{Circle} from './circle.js'
+const c = new Circle(10);
+c.draw();
